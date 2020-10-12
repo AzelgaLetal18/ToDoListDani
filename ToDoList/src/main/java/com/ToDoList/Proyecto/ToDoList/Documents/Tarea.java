800f3b734bf325cc045b80320477eacb0ff1f25a
AzelgaLetal18 <72132481+AzelgaLetal18@users.noreply.github.com>
2020-10-12 20:11:28 +0200

    Cambio Final

--- ToDoList/src/main/java/com/ToDoList/Proyecto/ToDoList/Documents/Tarea.java
+++ ToDoList/src/main/java/com/ToDoList/Proyecto/ToDoList/Documents/Tarea.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "tarea")
 public class Tarea {
+    
 
     @Id
     private String id = new ObjectId().toString();
